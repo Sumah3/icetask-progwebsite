1bba13810380aa5e58df9a644cb651a3ccb7c420
Courteney Young <coyoung@iie.ac.za>
2024-09-02 09:57:44 +0200

    Add project files.

---
+++ BlazorApp2/Program.cs
@@ -0,0 +1,39 @@
+
+
+namespace WebBasics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            
+
+            // Use default files like index.html
+            app.UseDefaultFiles();
+            // Enable serving static files from the wwwroot folder
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+
+
+            app.Run();
+        }
+    }
+}
