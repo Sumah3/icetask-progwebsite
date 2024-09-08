3cddc93952564319b947136e60ecd50d945edbda
Sumah3 <126692045+Sumah3@users.noreply.github.com>
2024-09-08 10:32:17 +0200

    added blazor animate and js doc, animated poster

---
+++ BlazorApp2/wwwroot/JavaScript.js
@@ -0,0 +1,8 @@
+﻿document.addEventListener('scroll', function () {
+    const elements = document.querySelectorAll('.scroll-animation');
+    elements.forEach(element => {
+        if (element.getBoundingClientRect().top < window.innerHeight) {
+            element.classList.add('in-view');
+        }
+    });
+});
